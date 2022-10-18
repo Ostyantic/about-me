ea98ef23254937545b1feb7829fc7c8f9c9fcfce
Anthony Lopez <Antl21292@gmail.com>
2022-10-17 22:47:59 -0400

    added a Javascript file

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let userAnswer1 = prompt('What is your name?');
+console.log(userAnswer1);
+
+let userAnswer2 = prompt('What state do you live in?');
+console.log(userAnswer2);
+
+let userAnswer3 = prompt('What is your favorite food?');
+console.log(userAnswer3);
+
+let userAnswer4 = prompt('What is your favorite color?');
+console.log(userAnswer4);
+
+alert('Hello, ' + userAnswer1 + ', in ' + userAnswer2 + '! ' + userAnswer3 + ' sound delicious! ' + userAnswer4 + ' is a really nice color!');
