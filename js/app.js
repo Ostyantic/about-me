c866035afba39f3bb50d36c7f89e887bf9138c0e
Anthony Lopez <Antl21292@gmail.com>
2022-10-24 20:03:25 -0400

    Added questions 6 & 7

--- js/app.js
+++ js/app.js
@@ -1,97 +1,164 @@
 'use strict';
 
-let usersName = prompt('What is your name?');
-console.log('The users name is: ', usersName);
-alert('Hello, ' + usersName + '! Welcome to my page! Let\'s break the ice with a little guessing game! Please answer "Yes/No" or "Y/N".');
+// Asking for the user's name, logging the user's name in to the console, and greeting the user
 
+let usersName = prompt('What is your name?');
+console.log('The user\'s name is: ', usersName);
+alert('Hello, ' + usersName + '! Welcome to my page! Let\'s break the ice with a little guessing game! For questions 1-5, please answer "Yes/No" or "Y/N".');
+
+// Starting the user with a score of 0
 let usersScore = 0;
 
-let userAnswer1 = prompt('Is playing video games a hobby of mine?');
-userAnswer1 = userAnswer1.toLowerCase();
-console.log('The users answer to question 1 is: ', userAnswer1);
+// Question 1
 
-if (userAnswer1 === 'yes' || userAnswer1 === 'y') {
+let question1 = prompt('Is playing video games a hobby of mine?');
+question1 = question1.toLowerCase();
+console.log(usersName + '\'s answer to question 1 is: ', question1);
+
+if (question1 === 'yes' || question1 === 'y') {
   alert('Ding! You are correct! I love playing video games!');
-  usersScore = usersScore + 1;
-} else if (userAnswer1 === 'no' || userAnswer1 === 'n') {
+  usersScore++;
+} else if (question1 === 'no' || question1 === 'n') {
   alert('Womp womp... Wrong! I love playing video games.');
 } else {
   alert('You did not answer "Yes/No" or "Y/N".');
 }
 
-let userAnswer2 = prompt('Do I have any pets?');
-userAnswer2 = userAnswer2.toLowerCase();
-console.log('The users answer to question 2 is: ', userAnswer2);
+// Question 2
 
-if (userAnswer2 === 'yes' || userAnswer2 === 'y') {
+let question2 = prompt('Do I have any pets?');
+question2 = question2.toLowerCase();
+console.log(usersName + '\'s answer to question 2 is: ', question2);
+
+if (question2 === 'yes' || question2 === 'y') {
   alert('You are right! I have 2 cats! Their names are Shadow and Onyx.');
-  usersScore = usersScore + 1;
-} else if (userAnswer2 === 'no' || userAnswer2 === 'n') {
+  usersScore++;
+} else if (question2 === 'no' || question2 === 'n') {
   alert('Sorry, you are wrong. I have 2 cats. Their names are Onyx and Shadow.');
 } else {
   alert('You did not answer "Yes/No" or "Y/N".');
 }
 
-let userAnswer3 = prompt('Do I like seafood?');
-userAnswer3 = userAnswer3.toLowerCase();
-console.log('The users answer to question 3 is: ', userAnswer3);
+// Question 3
 
-if (userAnswer3 === 'yes' || userAnswer3 === 'y') {
+let question3 = prompt('Do I like seafood?');
+question3 = question3.toLowerCase();
+console.log(usersName + '\'s answer to question 3 is: ', question3);
+
+if (question3 === 'yes' || question3 === 'y') {
   alert('Eww No. Seafood is not for me!');
-} else if (userAnswer3 === 'no' || userAnswer3 === 'n') {
+} else if (question3 === 'no' || question3 === 'n') {
   alert('Bingo! Fishy food is no bueno...');
-  usersScore = usersScore + 1;
+  usersScore++;
 } else {
   alert('You did not answer "Yes/No" or "Y/N.');
 }
 
-let userAnswer4 = prompt('Do I like math?');
-userAnswer4 = userAnswer4.toLowerCase();
-console.log('The users answer to question 4 is: ', userAnswer4);
+// Question 4
 
-if (userAnswer4 === 'yes' || userAnswer4 === 'y') {
+let question4 = prompt('Do I like math?');
+question4 = question4.toLowerCase();
+console.log(usersName + '\'s answer to question 4 is: ', question4);
+
+if (question4 === 'yes' || question4 === 'y') {
   alert('Yup! I love the logic behind math!');
-  usersScore = usersScore + 1;
-} else if (userAnswer4 === 'no' || userAnswer4 === 'n') {
+  usersScore++;
+} else if (question4 === 'no' || question4 === 'n') {
   alert('Wrong! I know a lot people hate math, but I love it!');
 } else {
   alert('You did not answer "Yes/No" or "Y/N.');
 }
 
-let userAnswer5 = prompt('Have I traveled outside of the country before?');
-userAnswer5 = userAnswer5.toLowerCase();
-console.log('The users answer to question 5 is: ', userAnswer5);
+// Question 5
 
-if (userAnswer5 === 'yes' || userAnswer5 === 'y') {
+let question5 = prompt('Have I traveled outside of the country before?');
+question5 = question5.toLowerCase();
+console.log(usersName + '\'s answer to question 5 is: ', question5);
+
+if (question5 === 'yes' || question5 === 'y') {
   alert('Sadly no... I would love to visit Tokyo, Japan for my first time!');
-} else if (userAnswer5 === 'no' || userAnswer5 === 'n') {
-  alert('You are correct, I haven\'t traveled yet but I will. I hope to go Tokyo, Japan one day!');
-  usersScore = usersScore + 1;
+} else if (question5 === 'no' || question5 === 'n') {
+  alert ('You are correct, I haven\'t traveled yet but I will. I hope to go Tokyo, Japan one day!');
+  usersScore++;
 } else {
-  alert('You did not answer "Yes/No" or "Y/N.');
+  alert ('You did not answer "Yes/No" or "Y/N.');
 }
+
+// Delcaring my number with the value 6 as a string and delcaring a variable question 6.
+
+const myNumber = '6';
+let question6;
+
+// Question 6 - This is a for loop that asks the user to guess a number that I am thinking. If the user guesses wrong, they are alerted and told how many guesses they have left. After 4 wrong guesses, the loop ends and the user is given the correct answer.
+
+for ( let i = 3; i >= 0; i--) {
+  let question6 = prompt('Can you guess the number I am thinking from 1 to 10?');
+  console.log(usersName + '\'s answer to question 6 is: ', question6);
+  if (question6 === myNumber) {
+    alert('Bingo! You guessed it!');
+    usersScore++;
+    break;
+  } else if (question6 < myNumber) {
+    alert('Too low, you have ' + i + ' guesses left!');
+  } else if (question6 > myNumber) {
+    alert('Too high, you have ' + i + ' guesses left!');
+  } else {
+    alert ('That\'s not a number!');
+  }
+}
+
+if (question6 !== myNumber) {
+  alert('The number I was thinking was: ' + myNumber);
+}
+
+// declaring a variable named favShow and assigning the value as an array of my favorite shows as strings.
+
+let favShow = ['bojack horseman', 'attack on titan', 'black sails', 'castlevania'];
+
+// for loop to log all of the items in the array in to the console
+
+for (let i = 0; i < favShow.length; i++) {
+  console.log(favShow[i]);
+}
+
+// Question 7 - This is a for loop that loops a max of 6 times. The user is asked to guess a favorite show of mine from a list provided on in the prompt window. If the user guesses wrong, they are alerted and told how many guesses they have left. After 6 wrong guesses, the loop ends.
+
+for (let i = 5; i > 0; i--){
+  let question7A = prompt('Which of the following shows are my favorite? Please choose one: "Game of Thrones", "Naruto", "BoJack Horseman", "The Walking Dead", "House of Cards", "Breaking Bad", "Attack on Titan", "Black Sails", "Orange is the New Black", "Castlevania"');
+  question7A = question7A.toLowerCase();
+  console.log(usersName + '\'s answer to question 7 is: ', question7A);
+
+  let question7B =  false;
+
+  for (let j = 0; j < favShow.length; j++) {
+    if (question7A === favShow[j]){
+      question7B = true;
+    }
+  }
+
+  if (question7B) {
+    alert('Yes! You are right!');
+    usersScore++;
+    break;
+  } else {
+    alert('No! You have ' + i + ' guesses left!');
+  }
+}
+
+// thanks the user for playing the game.
 
 alert('Thanks for playing, ' + usersName + '! I hope you had fun learning a little about me! Click "OK" to view your score!');
 
-if (usersScore === 5) {
+// alerts the user of their score
+
+if (usersScore === 7) {
   alert ('You guessed correctly on every question! Way to go!!');
-} else if (usersScore === 4) {
-  alert ('You guessed ' + usersScore + ' out of 5 questions correctly. Pretty good!');
-} else if (usersScore === 3) {
-  alert ('You guessed ' + usersScore + ' out of 5 questions correctly. Not too shabby.');
-} else if (usersScore === 2 || usersScore === 1) {
-  alert ('You guessed ' + usersScore + ' out of 5 questions correctly. You can do better than that!');
+} else if (usersScore === 5 || usersScore === 6) {
+  alert ('You guessed ' + usersScore + ' out of 7 questions correctly. Pretty good!');
+} else if (usersScore === 3 || usersScore === 4) {
+  alert ('You guessed ' + usersScore + ' out of 7 questions correctly. Not too shabby.');
+} else if (usersScore === 1 || usersScore === 2) {
+  alert ('You guessed ' + usersScore + ' out of 7 questions correctly. You can do better than that!');
 } else {
   alert('Oh no, you didnt guess any of the questions correctly...');
 }
-
-// let userAnswer2 = prompt('What is your favorite game to play?');
-// console.log(userAnswer2);
-
-// let userAnswer3 = prompt('What is your favorite food?');
-// console.log(userAnswer3);
-
-// let userAnswer4 = prompt('What is your favorite color?');
-// console.log(userAnswer4);
-
-// alert('Hello, ' + userAnswer1 + ', in ' + userAnswer2 + '! ' + userAnswer3 + ' sound delicious! ' + userAnswer4 + ' is a really nice color!');
